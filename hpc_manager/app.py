9d665fcb323e2da9e266adf5ac94e696e5117ba4
tamaraOIntegradevs <tamara+ovation@integradevs.com>
2017-11-22 10:04:44 -0300

    change make system method

--- hpc_manager/app.py
+++ hpc_manager/app.py
@@ -7,4 +7,4 @@ import hpc_manager.system as system
 level = logging.DEBUG if 'DEBUG_LOG' in os.environ else logging.INFO
 logging.basicConfig(stream=sys.stdout, level=level)
 
-application = system.make_api()
+application = system.make_system()
--- hpc_manager/system.py
+++ hpc_manager/system.py
@@ -7,27 +7,6 @@ from hpc_manager.slurm import submit_research_job
 
 
 def make_system():
-    # PubSub client
-    cb = pubsub.make_research_callback(submit_research_job,
-                                       client_id=config.secret('OVATION_CLIENT_ID'),
-                                       client_secret=config.secret('OVATION_CLIENT_SECRET'),
-                                       auth_domain=config.configuration('OVATION_AUTH_DOMAIN'),
-                                       audience=config.configuration('OVATION_AUDIENCE'),
-                                       ovation_api=config.configuration('OVATION_API_URL'),
-                                       head_node=config.configuration('CLUSTER_HEAD_NODE'),
-                                       key_filename=config.configuration('SSH_KEY_FILE'),
-                                       host_key_file=config.configuration('KNOWN_HOSTS_FILE'),
-                                       ssh_username=config.configuration('SSH_USERNAME'))
-
-    pubsub.open_subscription(config.configuration('GOOGLE_CLOUD_PROJECT_ID'),
-                             config.configuration('PUBSUB_REQUESTS_TOPIC'),
-                             subscription_name=config.configuration('PUBSUB_REQUEST_SUBSCRIPTION_NAME', None),
-                             callback=cb)
-
-    return None
-
-
-def make_api():
     api = falcon.API()
     api.add_route('/hpc_run', falcon.HTTP_200)
     # PubSub client
@@ -51,4 +30,5 @@ def make_api():
                          'callback': cb}).start()
 
     return api
+
 
