93d64575ffa29ae4e6bff504e279f9afb021dcb0
tamaraOIntegradevs <tamara+ovation@integradevs.com>
2017-12-14 14:48:58 -0300

    improve the ovation delivery script and update the jobs

--- cluster/ovation_core.sh
+++ cluster/ovation_core.sh
@@ -12,9 +12,9 @@ jid1=$(sbatch ~/bin/$VERSION/ovation_download_files.job $API_TOKEN $ACTIVITY_UUI
 
 echo "${jid1}"
 # multiple jobs can depend on a single job
-jid2=$(sbatch --dependency=afterany:$jid1 ~/bin/$VERSION/ovation_user.job $ACTIVITY_UUID $USER_IMAGE | cut -f4 -d' ')
+jid2=$(sbatch --dependency=afterany:$jid1 ~/bin/$VERSION/ovation_user.job $ACTIVITY_UUID $USER_IMAGE $job1 | cut -f4 -d' ')
 
 
-jid3=$(sbatch --dependency=afterany:$jid2 ~/bin/$VERSION/ovation_upload_result.job $API_TOKEN $ACTIVITY_UUID $jid2 $USER_IMAGE | cut -f4 -d' ')
+jid3=$(sbatch --dependency=afterany:$jid2 ~/bin/$VERSION/ovation_upload_result.job $API_TOKEN $ACTIVITY_UUID $jid2 $USER_IMAGE $job1 | cut -f4 -d' ')
 
 squeue
--- cluster/ovation_delivery.py
+++ cluster/ovation_delivery.py
@@ -2,63 +2,50 @@ import config as config
 from google.cloud import pubsub as gc_pubsub
 import json
 import argparse
+import logging
+import sys
+import os
+
+level = logging.DEBUG if 'DEBUG_LOG' in os.environ else logging.INFO
+logging.basicConfig(stream=sys.stdout, level=level)
 
 
-def send_result_error(args):
-    job_id = args.job_id
-    activity = args.activity_id
-    error_log = args.error
-    print(error_log)
-    message = {
-        "job_id": job_id,
-        "activity": activity,
-        "error_log": error_log
-    }
+def send_result(**args):
+    message = {}
+    topic = args.get('topic')
+
+    message['job_id'] = args.get('job_id')
+    message['activity_id'] = args.get('activity_id')
+    if args.get('error_log') is not None: message['error_log'] = args.get('error_log')
+
     data = json.dumps(message)
     data = data.encode('utf-8')
-    publisher = gc_pubsub.PublisherClient()
-    print(config.configuration('GOOGLE_APPLICATION_CREDENTIALS'))
-    print(config.configuration('GOOGLE_CLOUD_PROJECT_ID'))
-    print(config.configuration('PUBSUB_FAILURES_TOPIC'))
-    topic_path = publisher.topic_path(config.configuration('GOOGLE_CLOUD_PROJECT_ID'),
-                                      config.configuration('PUBSUB_FAILURES_TOPIC'))
 
-    publisher.publish(topic_path, data=data)
-    return "send_result_error"
-
-
-def send_result_success(args):
-    job_id = args.job_id
-    activity = args.activity_id
-    message = {
-        "job_id": job_id,
-        "activity": activity,
-    }
-    data = json.dumps(message)
-    data = data.encode('utf-8')
     publisher = gc_pubsub.PublisherClient()
     topic_path = publisher.topic_path(config.configuration('GOOGLE_CLOUD_PROJECT_ID'),
-                                      config.configuration('PUBSUB_SUCCESSES_TOPIC'))
+                                      topic)
 
     publisher.publish(topic_path, data=data)
-    return "send_result_success"
+    logging.info("The message sent to the topic {} is: {}".format(topic, json.dumps(message)))
 
 
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     parser_error = subparsers.add_parser('error', description='Send a error result')
-    parser_error.add_argument('-j', '--job_id',  help='Job id')
-    parser_error.add_argument('-a', '--activity_id',  help='Activity id')
-    parser_error.add_argument('-e', '--error',  help='Error log')
-    parser_error.set_defaults(func=send_result_error)
+    parser_error.add_argument('-j', '--job_id', help='Job id')
+    parser_error.add_argument('-a', '--activity_id', help='Activity id')
+    parser_error.add_argument('-e', '--error', help='Error log')
+    parser_error.set_defaults(topic=config.configuration('PUBSUB_FAILURES_TOPIC'))
 
     parser_success = subparsers.add_parser('success', description='Send a success result')
     parser_success.add_argument('-j', '--job_id', help='Job id')
     parser_success.add_argument('-a', '--activity_id', help='Activity id')
-    parser_success.set_defaults(func=send_result_success)
+    parser_success.set_defaults(topic=config.configuration('PUBSUB_SUCCESSES_TOPIC'))
+
     args = parser.parse_args()
-    args.func(args)
+    send_result(topic=args.topic, job_id=args.job_id, activity_id=args.activity_id, error_log=args.error)
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
