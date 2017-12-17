71718a92ab8c55404596f81376991255c067ac3d
Tamara Ortiz <tamara.tfs@gmail.com>
2017-12-17 17:20:22 -0300

    add route falcon api, in main

---
+++ hpc_manager/main.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+import os
+import hpc_manager.app as app
+import hpc_manager.system as system
+import falcon
+
+
+app.application.add_route('/hpc_run', app.HpcHandler())
+
+
+if __name__ == '__main__':
+    level = logging.DEBUG if 'DEBUG_LOG' in os.environ else logging.INFO
+    logging.basicConfig(stream=sys.stdout, level=level)
+    httpd = falcon.simple_server.make_server('127.0.0.1', 8000, app.application)
+    httpd.serve_forever()
