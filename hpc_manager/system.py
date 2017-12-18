2955be3bc5590ce675544b6ef4f9325647605d4c
Tamara Ortiz <tamara.tfs@gmail.com>
2017-12-18 18:43:23 -0300

    add pubsub as comment

--- hpc_manager/system.py
+++ hpc_manager/system.py
@@ -1,10 +1,28 @@
 import falcon
+import hpc_manager.app as app
 
 
 def make_system():
     application = falcon.API()
     application.add_route('/hpc_run', app.HpcHandler())
 
-    # TODO should set up pubsub here
+    # cb = pubsub.make_research_callback(submit_research_job,
+    #                                    client_id=config.secret('OVATION_CLIENT_ID'),
+    #                                    client_secret=config.secret('OVATION_CLIENT_SECRET'),
+    #                                    auth_domain=config.configuration('OVATION_AUTH_DOMAIN'),
+    #                                    audience=config.configuration('OVATION_AUDIENCE'),
+    #                                    ovation_api=config.configuration('OVATION_API_URL'),
+    #                                    head_node=config.configuration('CLUSTER_HEAD_NODE'),
+    #                                    key_filename=config.configuration('SSH_KEY_FILE'),
+    #                                    host_key_file=config.configuration('KNOWN_HOSTS_FILE'),
+    #                                    ssh_username=config.configuration('SSH_USERNAME'))
+    #
+    # threading.Thread(target=pubsub.open_subscription,
+    #                  name='pubsub-subscription',
+    #                  args=(config.configuration('GOOGLE_CLOUD_PROJECT_ID'),
+    #                        config.configuration('PUBSUB_REQUESTS_TOPIC')),
+    #                  kwargs={
+    #                      'subscription_name': config.configuration('PUBSUB_REQUEST_SUBSCRIPTION_NAME', None),
+    #                      'callback': cb}).start()
 
     return application
