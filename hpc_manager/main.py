3accc2699f16c5ff306d5f590cbb2e0f61fa2d8d
Barry Wark <barrywark@gmail.com>
2017-12-27 00:57:48 -0500

    Use debug log by default

--- hpc_manager/main.py
+++ hpc_manager/main.py
@@ -4,7 +4,7 @@ import os
 import hpc_manager.system as system
 import falcon
 
-level = logging.DEBUG if 'DEBUG_LOG' in os.environ else logging.INFO
+level = logging.DEBUG #if 'DEBUG_LOG' in os.environ else logging.INFO
 logging.basicConfig(stream=sys.stdout, level=level)
 
 application = system.make_system()
