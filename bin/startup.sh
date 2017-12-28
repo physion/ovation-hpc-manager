66d644418dc370ed4bbbab123289c1b820eb937e
Tamara Ortiz <tamara.tfs@gmail.com>
2017-12-28 08:46:03 -0300

    add vpn config

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,9 +4,13 @@ OVPN_PATH=$1
 
 # Start VPN. /var/secrets/vpn/credentials should be username\npassword\n
 
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
 #printf '%s\n' $VPN_USERNAME $VPN_PASSWORD > /etc/openvpn/credentials
 #sed -i 's/auth-user-pass/auth-user-pass \/etc\/openvpn\/credentials/g' $OVPN_PATH
-openvpn --config $OVPN_PATH &
+openvpn --config $OVPN_PATH --daemon .
 
 
 # Start HPC manager
