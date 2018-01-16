d4730154cf8743daabdf37f8a11adc1134f01f84
tamaraOIntegradevs <tamara+ovation@integradevs.com>
2018-01-16 09:55:20 -0300

    add ovation cli args

--- cluster/ovation_core.sh
+++ cluster/ovation_core.sh
@@ -3,17 +3,18 @@
 API_TOKEN=$1
 ACTIVITY_UUID=$2
 USER_IMAGE=$3
+OVATION_CLI_ARGS=$4
 VERSION="1.0.0"
 
 
 
 # first job - no dependencies
-jid1=$(sbatch ~/bin/$VERSION/ovation_download_files.job $API_TOKEN $ACTIVITY_UUID $USER_IMAGE | cut -f4 -d' ')
+jid1=$(sbatch ~/bin/$VERSION/ovation_download_files.job $API_TOKEN $ACTIVITY_UUID $USER_IMAGE $OVATION_CLI_ARGS | cut -f4 -d' ')
 
 # multiple jobs can depend on a single job
 jid2=$(sbatch --dependency=afterany:$jid1 ~/bin/$VERSION/ovation_user.job $ACTIVITY_UUID $USER_IMAGE $jid1 | cut -f4 -d' ')
 
 
-jid3=$(sbatch --dependency=afterany:$jid2 ~/bin/$VERSION/ovation_upload_result.job $API_TOKEN $ACTIVITY_UUID $jid2 $USER_IMAGE $jid1 | cut -f4 -d' ')
+jid3=$(sbatch --dependency=afterany:$jid2 ~/bin/$VERSION/ovation_upload_result.job $API_TOKEN $ACTIVITY_UUID $jid2 $USER_IMAGE $jid1 $OVATION_CLI_ARGS | cut -f4 -d' ')
 
 squeue
