fb44cf48040d088ec892417cfb0110cb0c940abc
tamaraOIntegradevs <tamara+ovation@integradevs.com>
2018-01-26 17:01:57 -0300

    fix overwrite resolv.conf

--- bin/resolvconf-up.sh
+++ bin/resolvconf-up.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "nameserver 10.20.30.98" > /etc/resolv.conf
-echo "nameserver 10.20.30.2" > /etc/resolv.conf
+echo "nameserver 10.20.30.98" >> /etc/resolv.conf
+echo "nameserver 10.20.30.2" >> /etc/resolv.conf
 echo "nameserver 10.20.30.3" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "search alphacruncher.net" >> /etc/resolv.conf
