cc08a87ce7a2f96c3f2d5ce949bb29b3f3f2f640
Barry Wark <barrywark@gmail.com>
2018-04-27 03:14:28 -0400

    Oh, so many fixes for /cluster assets

--- cluster/deploy_cluster.sh
+++ cluster/deploy_cluster.sh
@@ -2,11 +2,12 @@
 
 VERSION=$1
 
+rm *.img
+
 sudo singularity build ovation_download_files.img ovation_download_files.def
 sudo singularity build ovation_upload_result.img ovation_upload_result.def
 
-scp ovation_download_files.img ovation@scc.alphacruncher.net:~ #/cm/shared/sing-images/ovation/
-scp ovation_upload_result.img ovation@scc.alphacruncher.net:~ #/cm/shared/sing-images/ovation/
+scp *.img ovation@scc.alphacruncher.net:/cm/shared/sing-images/ovation/
 
 scp ovation_core.sh ovation@scc.alphacruncher.net:~/bin/$VERSION/
 scp ovation_delivery.py ovation@scc.alphacruncher.net:~/bin/$VERSION/
